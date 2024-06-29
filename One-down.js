accd922664f816349a781134de152a0f75f52024
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-28 22:52:15 -0700

    Update One-down.js

--- One-down.js
+++ One-down.js
@@ -21,3 +21,15 @@ function oneDown(str) {
 }
 
 // or
+
+const oneDown = str => {
+  if (typeof str !== 'string') return 'Input is not a string'
+
+  return str.replace(/[a-z]/gi, match => {
+    const code = match.codePointAt(0)
+
+    if (code === 97) return 'Z'
+    else if (code === 65) return 'z'
+    else return String.fromCodePoint(code - 1)
+  })
+}
